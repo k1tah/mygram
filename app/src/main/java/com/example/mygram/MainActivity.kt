7056f5db1bf9d9dee590e80517ab1a88fb027d18
Nikita Cherentaev <nicita.cr@gmail.com>
2022-05-18 19:26:52 +0300

    Enable viewBinding

--- app/src/main/java/com/example/mygram/MainActivity.kt
+++ app/src/main/java/com/example/mygram/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.mygram
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.mygram.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var bindingClass: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        bindingClass = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(bindingClass.root)
     }
 }
