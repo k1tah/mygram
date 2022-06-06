e1e98d62917b7a8d5d71dac8ff36e8f00978ff1f
Nikita Cherentaev <nicita.cr@gmail.com>
2022-06-06 20:26:40 +0300

    небольшие изменения в коде. добавлен объект Const

---
+++ app/src/main/java/Const.kt
@@ -0,0 +1,3 @@
+object Const {
+    val testTag = "test"
+}
--- app/src/main/java/com/example/mygram/activity/MainActivity.kt
+++ app/src/main/java/com/example/mygram/activity/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.mygram.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.example.mygram.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -12,5 +13,9 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         bindingClass = ActivityMainBinding.inflate(layoutInflater)
         setContentView(bindingClass.root)
+        val list = listOf(1, 2, 3, 4, 5)
+        list.forEach {
+            Log.d(Const.testTag, "$it")
+        }
     }
 }
