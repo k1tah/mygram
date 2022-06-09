32f58bc047c8c65181c1bc86d24852ee1234011b
Nikita Cherentaev <nicita.cr@gmail.com>
2022-06-09 20:45:19 +0300

    добавлен RecyclerView

---
+++ app/src/main/java/com/example/mygram/activity/adapter/MsdAdapter.kt
@@ -0,0 +1,25 @@
+package com.example.mygram.activity.adapter
+
+import android.content.Context
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class MsdAdapter(context: Context): RecyclerView.Adapter<MsdAdapter.MsgViewHolder>() {
+
+    class MsgViewHolder(view: View): RecyclerView.ViewHolder(view){
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MsgViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: MsgViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+}
---
+++ app/src/main/java/com/example/mygram/activity/model/MsgModel.kt
@@ -0,0 +1,3 @@
+package com.example.mygram.activity.model
+
+data class MsgModel(val message: String)
