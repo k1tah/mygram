1e702fbd799ce28b692c8329941b5f7a76e007be
Nikita Cherentaev <nicita.cr@gmail.com>
2022-06-20 11:51:04 +0300

    исправления в меню 2

--- app/src/main/java/com/example/mygram/activity/MainActivity.kt
+++ app/src/main/java/com/example/mygram/activity/MainActivity.kt
@@ -3,19 +3,27 @@ package com.example.mygram.activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
+import com.example.mygram.R
 import com.example.mygram.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var bindingClass: ActivityMainBinding
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         bindingClass = ActivityMainBinding.inflate(layoutInflater)
         setContentView(bindingClass.root)
-        val list = listOf(1, 2, 3, 4, 5)
-        list.forEach {
-            Log.d(Const.testTag, "$it")
+
+        val toolbar = bindingClass.toolbar
+        val drawerLayout = bindingClass.drawerLayout
+
+        toolbar.setNavigationOnClickListener {
+            drawerLayout.open()
         }
     }
+
 }
