a1e5e1e9c34366d2f30cc364c0396cb823706889
Nikita Cherentaev <nicita.cr@gmail.com>
2022-06-20 11:51:38 +0300

    исправления в меню 2

--- app/src/main/java/com/example/mygram/activity/MainActivity.kt
+++ app/src/main/java/com/example/mygram/activity/MainActivity.kt
@@ -2,10 +2,6 @@ package com.example.mygram.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.view.MenuItem
-import androidx.appcompat.app.ActionBarDrawerToggle
-import com.example.mygram.R
 import com.example.mygram.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
